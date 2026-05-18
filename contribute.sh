5c5c2c4c05e291906292e7383ea93e774a73ee15
Jsinco <jonah@jsinco.dev>
2026-05-18 14:36:45 -0400

    add wonderland assets

--- contribute.sh
+++ contribute.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-
 if ! command -v git &> /dev/null; then
   echo "Error: Git is not installed or not found in PATH."
   exit 1
 fi
 
-
-if [ -z "$1" ]; then
+if [ $# -eq 0 ]; then
   echo "Usage: $0 <commit-message>"
   exit 1
 fi
 
 git add --all
-git commit -m "$1"
+git commit -m "$*"
 git push
